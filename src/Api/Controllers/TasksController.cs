28a3a84a08ac4253e61dd481d30acac6edad1ad0
letrananhvu <vule5726@gmail.com>
2023-12-27 15:20:38 +0200

    Refactor

---
+++ src/Api/Controllers/TasksController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api;
+
+[ApiController]
+[Authorize]
+[Route("api/[controller]")]
+public class TasksController: ControllerBase
+{
+    [HttpGet]
+    public string Get()
+    {
+        return "sakdfkasdf";
+    }
+}
