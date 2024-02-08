ecb447e4986882220fc719adf563e592064ddfa9
Le Tran Anh Vu <vule5726@gmail.com>
2024-02-08 14:26:53 +0200

    Refactor

--- src/Application/DTOs/CreateTodoDto.cs
+++ src/Application/DTOs/CreateTodoDto.cs
@@ -3,19 +3,10 @@ using Domain.Models;
 
 namespace Application.DTOs;
 
-public class CreateNestedSubTodoDto
-{
-    public required string Name { get; set; }
-    public ICollection<CreateTodoDto>? SubTodos { get; set; }
-    
-    [EnumDataType(typeof(RepeatableType))]
-    public RepeatableType? RepeatableType { get; set; } 
-}
-
 public class CreateTodoDto
 {
     public required string Name { get; set; }
-    public ICollection<CreateNestedSubTodoDto>? SubTodos { get; set; }
+    public ICollection<UpsertNestedSubTodoDto>? SubTodos { get; set; }
     
     [EnumDataType(typeof(RepeatableType))]
     public RepeatableType? RepeatableType { get; set; } 
--- src/Application/DTOs/UpdateSubTodoDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using Domain.Models;
-
-namespace Application.DTOs;
-
-public class UpdateSubTodoDto
-{
-    public required Guid ParentId { get; set; }
-    public required string Name { get; set; }
-}
